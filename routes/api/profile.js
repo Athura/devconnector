ef282f5171f9d22e25b2a0be3649fb8543206533
Athura <joshualjohnson33@gmail.com>
2019-04-29 19:40:01 -0400

    finished profile model, begin profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route     GET api/profile
-// @desc      Test route for profile
-// @access    public
-router.get('/', (req, res) => res.send('Profile route'));
+// @route     GET api/me
+// @desc      Get current user's profile
+// @access    private
+router.get('/', auth, async (req, res) => {
+    try {
+        const profile = await Profile
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send("Server Error :D");
+    }
+})
 
 module.exports = router;
