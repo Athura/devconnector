2367d52af34db0f8b65f3a623c0fcdbb834a3a2e
Athura <joshualjohnson33@gmail.com>
2019-05-07 08:06:11 -0400

    Finish getting profiles
    - actions
    - reducers
    - types

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,11 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS
+} from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -19,6 +26,18 @@ export default function(state = initialState, action) {
         profile: payload,
         loading: false
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -27,11 +46,11 @@ export default function(state = initialState, action) {
       };
     case CLEAR_PROFILE:
       return {
-          ...state,
-          profile: null,
-          repos: [],
-          loading: false
-      }
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false
+      };
     default:
       return state;
   }
