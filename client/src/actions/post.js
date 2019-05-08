98f251809ab68f929d9726b52f9c62430dde667a
Athura <joshualjohnson33@gmail.com>
2019-05-08 07:46:21 -0400

    Finish create post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST
+} from "./types";
 
 export const getPosts = () => async dispatch => {
   try {
@@ -22,67 +28,91 @@ export const getPosts = () => async dispatch => {
 };
 
 export const addLike = id => async dispatch => {
-    try {
-      const res = await axios.put(`/api/posts/like/${id}`);
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
 
-      dispatch({
-        type: UPDATE_LIKES,
-        payload: {
-            id,
-            likes: res.data
-        }
-      });
-    } catch (err) {
-      dispatch({
-        type: POST_ERROR,
-        payload: {
-          msg: err.response.statusText,
-          state: err.response.status
-        }
-      });
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: {
+        id,
+        likes: res.data
+      }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        state: err.response.status
+      }
+    });
+  }
+};
+
+export const removeLike = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/unlike/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: {
+        id,
+        likes: res.data
+      }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        state: err.response.status
+      }
+    });
+  }
+};
+
+export const deletePost = id => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id
+    });
+    dispatch(setAlert("Post removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        state: err.response.status
+      }
+    });
+  }
+};
+
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
     }
   };
 
-  export const removeLike = id => async dispatch => {
-    try {
-      const res = await axios.delete(`/api/posts/unlike/${id}`);
+  try {
+    const res = await axios.post("/api/posts/", formData, config);
 
-      dispatch({
-        type: UPDATE_LIKES,
-        payload: {
-            id,
-            likes: res.data
-        }
-      });
-    } catch (err) {
-      dispatch({
-        type: POST_ERROR,
-        payload: {
-          msg: err.response.statusText,
-          state: err.response.status
-        }
-      });
-    }
-  };
-
-  export const deletePost = id => async dispatch => {
-    try {
-      await axios.delete(`/api/posts/${id}`);
-
-      dispatch({
-        type: DELETE_POST,
-        payload: id
-      });
-      dispatch(
-          setAlert('Post removed', 'success')
-      )
-    } catch (err) {
-      dispatch({
-        type: POST_ERROR,
-        payload: {
-          msg: err.response.statusText,
-          state: err.response.status
-        }
-      });
-    }
-  };
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+    dispatch(setAlert("Post created", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        state: err.response.status
+      }
+    });
+  }
+};
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,26 +4,30 @@ import { connect } from "react-redux";
 import { getPosts } from "../../actions/post";
 
 import Spinner from "../layout/Spinner";
-import PostItem from './PostItem';
+import PostItem from "./PostItem";
+import PostFrom from "./PostForm";
+import PostForm from "./PostForm";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
-    useEffect(() => {
-        getPosts();
-    }, [getPosts])
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
 
-  return loading ? <Spinner /> : (
-      <Fragment>
-          <h1 className="large text-primary">Posts</h1>
-          <p className="lead">
-            <i className="fas fa-user"></i> Welcome to the community
-          </p>
-          {/* Post Form */}
-          <div className="posts">
-            {posts.map(post => (
-                <PostItem key={post._id} post={post} />
-            ))}
-          </div>
-      </Fragment>
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to the community
+      </p>
+      <PostForm />
+      <div className="posts">
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
   );
 };
 
